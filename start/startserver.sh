1d098fbcf48277f2a929b3fc2f46a4a6b8abd489
QbusKoen <54985218+QbusKoen@users.noreply.github.com>
2019-09-07 05:05:53 +0200

    Update startserver.sh

--- start/startserver.sh
+++ start/startserver.sh
@@ -4,7 +4,7 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>/home/pi/bootlog.log 2>&1
 # Everything below will go to the file 'log.out':
-cd /home/pi/QbusServer
+cd /home/pi/QbusOpenHab/QbusServer
 mono QServer.exe &
 
 
